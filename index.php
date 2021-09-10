58818af98253622c873e5ffdf31a193695540184
Al-Mamun <67108634+mamun2576@users.noreply.github.com>
2021-09-10 21:32:25 +0600

    oop php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//here is a class 
+class mobile{
+    //It is public function
+    //we use A method called show () has been designed in the class.
+    
+    public function show(){
+        echo "Method have been added to the class here";
+    }
+}
+
+
+?>
