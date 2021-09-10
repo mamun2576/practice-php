1b3f95769a4bfa94f3dab5f392db4188b5969094
Al-Mamun <67108634+mamun2576@users.noreply.github.com>
2021-09-10 21:41:40 +0600

    Add files via upload

---
+++ access_property.php
@@ -0,0 +1,21 @@
+<?php
+
+//here is a class
+
+class mobile{
+
+    //It is public function
+
+    //we use A method called show () has been designed in the class.
+    
+    public $show = "Method have been added to the class here";
+    
+}
+//now I will create object
+$obj1=new mobile();
+
+//now I will access the property through the object
+echo $obj1->show;
+
+
+?>
